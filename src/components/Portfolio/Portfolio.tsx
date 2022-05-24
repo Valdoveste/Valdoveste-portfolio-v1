650c6233164e9214fd8a29505b9203e4dc3d738f
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-24 01:40:02 -0300

    Portfolio done, BEM, text and card elements.

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -4,19 +4,21 @@ const Portfolio: React.FC = () => {
     return (
         <section id="section-portfolio">
             <div>
-                <h1>
+                <h1 className="section-portfolio__title">
                     My Portfolio
                 </h1>
-                <p>
-                    my projects
+                <p className="section-portfolio__text">
+                    A small collection of my recent projects there were all selected by me.
+                    Most of them I've done with the help of incredible people.
                 </p>
 
-                <div>
+                <div id="card-container">
                     <article className="card">
-                        <span className="card__overlay"></span>
+                        <span className="card__overlay">
+                            <a href="https://github.com/Valdoveste/nebulum" target={"_blank"}></a>
+                        </span>
                         <div className="card__content">
-                            <img src="" alt="" className="card__content__image" />
-
+                            <img src="https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png" alt="" className="card__content__image" />
                         </div>
                     </article>
                 </div>
