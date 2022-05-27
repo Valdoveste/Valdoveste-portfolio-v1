01590943e008d06294977eda76d1479267561a08
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-26 22:27:35 -0300

    About Cards

--- src/cards.tsx
+++ src/cards.tsx
@@ -14,6 +14,72 @@ export const AboutCards = [
         url: "https://github.com/Valdoveste/nebulum",
         logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
     },
+    {
+        id: 1,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 2,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 3,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 4,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 5,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 6,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 7,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 8,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 9,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 10,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    },
+    {
+        id: 11,
+        logo_alt: "Nebulum Logo",
+        url: "https://github.com/Valdoveste/nebulum",
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+    }
 ];
 
 export default PortfolioCards;
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,16 +1,37 @@
 import React from "react";
+import { AboutCards } from "../../cards";
 
 const About: React.FC = () => {
+    const renderCards = (): JSX.Element[] => {
+        return AboutCards.map((card) => {
+            return (
+                <article className="card" key={card.id}>
+                    <span className="card__overlay">
+                        <a href={card.url} target={"_blank"}></a>
+                    </span>
+                    <div className="card__content">
+                        <img src={card.logo} alt={card.logo_alt} className="card__content__image" />
+                    </div>
+                </article>
+            );
+        });
+    }
+
     return (
         <section id="section-about">
-            <div>
-                <h1>
-                    My, my skills
+            <article>
+                <h1 className="section-about__title">
+                    My About
                 </h1>
-                <p>
-                    my skills
+                <p className="section-about__text">
+                    A small collection of my recent projects there were all selected by me.
+                    Most of them I've done with the help of incredible people.
                 </p>
-            </div>
+
+            </article>
+                <div id="card-container">
+                    {renderCards()}
+                </div>
         </section>
     );
 }
