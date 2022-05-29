ff39548ba0099735175b4a9a5f3d4aba70caa706
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-29 01:13:09 -0300

    Section home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import Intro from "../../assets/Intro.mp4";
 
 const Home: React.FC = () => {
     return (
         <section id="section-home">
-            <article>
-                <h1>
-                    Hi,
+            <video key={1} className="section-home__image" autoPlay loop>
+                <source src={Intro} type="video/mp4"></source>
+                Your browser does not support this video, video source below.
+                https://pt.videezy.com/fundos/5054-fluxo-de-fundo-de-movimento-4k-das-chamas
+            </video>
+            <article className="section-home__content">
+                <h1 className="section-home__content__title">
+                    Hi, I'm Valdoveste
                 </h1>
-                <h1>
-                    I'm nValdoveste
-                </h1>
-
-                <p>
+                <p className="section-home__content__text">
                     FullStack Developer
                 </p>
             </article>
