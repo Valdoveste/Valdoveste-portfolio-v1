a0939a51e68d9f6537c77e42a467e20cedf4452d
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-29 01:25:26 -0300

    Modal, still just a test, probable will be deleted.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 
 const Modal: React.FC = () => {
-    return(
-        <div className="modal">
-            <div className="modal__header">
-                <p className="modal__header_title"></p>
+    return (
+        <>
+            <div className="modal-overlay"></div>
+            <div className="modal">
+                <div className="modal__content">
+                    <div className="modal__content__showcase">
+                        <img alt="" className="modal__content__showcase__image" />
+                    </div>
+                    <div className="modal__content__info">
+                        <p className="modal__content__text">
+                            An online album with the objective to be simple, clean,
+                            and straight to the point. Consuming the Pexels API to load
+                            a massive amount of different photos. One of the main objectives
+                            of this application was to learn more about clean & simple design
+                            also have extra care for the mobile users, applying concepts as infinityScroll
+                            and FAB(Floating Action Button) and modal that works for portrait and landscape
+                            screen orientations.
+                        </p>
+                        <div className="modal__content__technologies">
+                            <ul className="modal__content__technologies__list">
+                                <li className="modal__content__list__item">JavaScript</li>
+                                <li className="modal__content__list__item">HTML5</li>
+                                <li className="modal__content__list__item">CSS3</li>
+                                <li className="modal__content__list__item">SASS</li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <a href="" className="modal__content__page__link">Nebulum.com</a> */}
+                </div>
             </div>
-
-            <div className="modal__content">
-                
-            </div>
-        </div>
+        </>
     );
 }
 
