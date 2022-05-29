e3bc7dfb7aae01cb570d9026daa65859ea119a06
Valdoveste <caio.valdoveste@outlook.com.br>
2022-05-29 01:30:59 -0300

    data file for the cards

--- src/cards.tsx
+++ src/cards.tsx
@@ -3,17 +3,84 @@ const PortfolioCards = [
         id: 0,
         logo_alt: "Nebulum Logo",
         url: "https://github.com/Valdoveste/nebulum",
-        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png",
+        projec_desc: "An online album with the objective to be simple, clean, and straight to the point. Consuming the Pexels API to load a massive amount of different photos. One of the main objectives of this application was to learn more about clean & simple design also have extra care for the mobile users, applying concepts as infinityScroll and FAB(Floating Action Button) and modal that works for portrait and landscape screen orientations."
     },
 ];
 
 export const AboutCards = [
     {
         id: 0,
-        logo_alt: "Nebulum Logo",
-        url: "https://github.com/Valdoveste/nebulum",
-        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png"
+        text: "React",
+        url: "",
+        logo: ""
     },
+    {
+        id: 1,
+        text: "HTML",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 2,
+        text: "CSS3",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 3,
+        text: "SASS",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 4,
+        text: "BEM",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 5,
+        text: "JavaScript",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 6,
+        text: "TypeScript",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 7,
+        text: "C",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 8,
+        text: "NodeJS",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 9,
+        text: "Git",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 10,
+        text: "GitFlow",
+        url: "",
+        logo: ""
+    },
+    {
+        id: 11,
+        text: "SQL",
+        url: "",
+        logo: ""
+    }
 ];
 
 export default PortfolioCards;
