c4a0adfbb6e1ae4dfe7b41dffe85d9ee5836410f
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-01 22:34:10 -0300

    Change of plans,  I'll do this 3d "block" in blender

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,20 +1,6 @@
 import React from "react";
-// import { AboutCards } from "../../cards";
 
 const About: React.FC = () => {
-    // const renderCards = (): JSX.Element[] => {
-    //     return AboutCards.map((card) => {
-    //         return (
-    //             <article className="card" key={card.id}>
-    //                 <span className="card__overlay"></span>
-    //                 <div className="card__content">
-    //                     <p className="card__content__text">{card.text}</p>
-    //                 </div>
-    //             </article>
-    //         );
-    //     });
-    // }
-
     return (
         <section id="section-about">
             <article className="section-about__content">
@@ -27,34 +13,6 @@ const About: React.FC = () => {
                 </p>
 
             </article>
-
-            <div id="panda">
-                <div className="scene2">
-                    <div className="box show-bottom">
-                        <div className="box__face box__face--front"></div>
-                        <div className="box__face box__face--back"></div>
-                        <div className="box__face box__face--right">right</div>
-                        <div className="box__face box__face--left"></div>
-                        <div className="box__face box__face--top">top</div>
-                        <div className="box__face box__face--bottom"></div>
-                    </div>
-                </div>
-                <div className="scene">
-                    <div className="box show-bottom">
-                        <div className="box__face box__face--front"></div>
-                        <div className="box__face box__face--back">
-                        </div>
-                        <div className="box__face box__face--right">right</div>
-                        <div className="box__face box__face--left"></div>
-                        <div className="box__face box__face--top">top</div>
-                        <div className="box__face box__face--bottom"></div>
-                    </div>
-                </div>
-            </div>
-
-            {/* <div id="card-container">
-                    {renderCards()}
-                </div> */}
         </section>
     );
 }
