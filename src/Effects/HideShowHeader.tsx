b3ac0e55cc109d4dda0bc73569799c09d031fde0
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 02:19:39 -0300

    Checbox animation

--- src/Effects/HideShowHeader.tsx
+++ src/Effects/HideShowHeader.tsx
@@ -1,11 +1,14 @@
 // Credits: Gary Simon - DesignCourse
 export default function hideAndShowHeader() {
     const header = document.querySelector('.header__navbar')!;
+    const checkbox = document.getElementById('menuCheck') as HTMLInputElement | null;
     let lastScroll = 0;
 
     window.addEventListener("scroll", () => {
         const currentScroll = window.pageYOffset;
         if (currentScroll <= 0) {
+            if (checkbox != null)
+                checkbox.checked = false;
             header.classList.remove("scroll-up");
             return;
         }
@@ -17,6 +20,8 @@ export default function hideAndShowHeader() {
             currentScroll < lastScroll &&
             header.classList.contains("scroll-down")
         ) {
+            if (checkbox != null)
+                checkbox.checked = false;
             header.classList.remove("scroll-down");
             header.classList.add("scroll-up");
         }
