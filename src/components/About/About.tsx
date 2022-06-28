3cdc7ea712fe9c0d495ab02a4595a02d31a84405
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 02:16:35 -0300

    Better responsive section and start for cube animation

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,157 +1,77 @@
 import React from "react";
+import Cube from '../Cube/Cubes'
+
+window.addEventListener("load", (e) => {
+    let flag = true;
+    const about = document.getElementById('about')!;
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0 && flag) {
+                const txt = document.querySelectorAll('.cube__outter')[0];
+                txt.classList.add("txt");
+            }
+        });
+    });
+
+    observer.observe(about)
+})
 
 const About: React.FC = () => {
     return (
         <section id="about">
             <article className="about__content">
                 <h1 className="about__title">
-                    My About
+                    About Me
                 </h1>
                 <p className="about__text">
                     Olá, me chamo Caio Valdoveste e eu gosto de desafios desde que
                     tive meu primeiro contato com programação em 2019 fiquei fascinado
                     com o quanto essa área é váriada, então após algum tempo realizando pequenos
-                    projetos em outras linguagens. Tive meu primeiro contato com WEB onde percebi que 
-                    tinha um apreço pela construção não apenas das funcionalidade mas também pelo como 
+                    projetos em outras linguagens. Tive meu primeiro contato com WEB onde percebi que
+                    tinha um apreço pela construção não apenas das funcionalidade mas também pelo como
                     o conteudo era apresentado para o usuário.
                 </p>
-                
-                <p className="about__text">
 
+                <p className="about__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque lobortis eleifend tellus, ut fermentum mauris luctus ac.
+                    Pellentesque lobortis eleifend tellus, ut fermentum mauris luctus ac.
+                    Cras finibus quis ante id interdum.
+                </p>
+
+                <p className="about__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 </p>
 
                 <div className="about__technologies">
                     <ul className="about__technologies__list">
-                        <li className="technologies__item">Familiar</li>
+                        <li className="technologies__item">Familiar:</li>
                         <li className="technologies__item">JavaScript</li>
                         <li className="technologies__item">HTML</li>
                         <li className="technologies__item">CSS</li>
                         <li className="technologies__item">SASS</li>
                         <li className="technologies__item">React</li>
+                        <li className="technologies__item">Git</li>
+                        <li className="technologies__item">GitFlow</li>
+                        <li className="technologies__item">MySQL</li>
                     </ul>
                     <ul className="about__technologies__list">
-                        <li className="technologies__item">Não tão familiar</li>
+                        <li className="technologies__item">Aprendendo:</li>
                         <li className="technologies__item">TypeScript</li>
                         <li className="technologies__item">NodeJS</li>
                     </ul>
                     <ul className="about__technologies__list">
-                        <li className="technologies__item">Pretendo aprender</li>
+                        <li className="technologies__item">Pretendo aprender:</li>
+                        <li className="technologies__item">Python</li>
+                        <li className="technologies__item">NextJS</li>
                     </ul>
                 </div>
 
             </article>
             <article className="technologies__cubes">
-                <div className="scene2">
-                    <div className="cube__outter">
-                        <div className="cube__face cube__face--front">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__A"></div>
-                        <div className="cube__face corner__B"></div>
-                        <div className="cube__face cube__face--back">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__C"></div>
-                        <div className="cube__face corner__D"></div>
-                        <div className="cube__face cube__face--right">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--left">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__E"></div>
-                        <div className="cube__face corner__F"></div>
-                        <div className="cube__face cube__face--top">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--bottom">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__G"></div>
-                        <div className="cube__face corner__H"></div>
-                    </div>
-                </div>
-
-                <div className="scene1">
-                    <div className="cube__inner">
-                        <div className="cube__face cube__face--front">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__A"></div>
-                        <div className="cube__face corner__A"></div>
-                        <div className="cube__face corner__B"></div>
-                        <div className="cube__face cube__face--back">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__C"></div>
-                        <div className="cube__face corner__D"></div>
-                        <div className="cube__face cube__face--right">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--left">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__E"></div>
-                        <div className="cube__face corner__F"></div>
-                        <div className="cube__face cube__face--top">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--bottom">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__G"></div>
-                        <div className="cube__face corner__H"></div>
-                    </div>
-                </div>
-
-                <div className="scene3">
-                    <div className="cube__inner__inner">
-                        <div className="cube__face cube__face--front">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__A"></div>
-                        <div className="cube__face corner__B"></div>
-                        <div className="cube__face cube__face--back">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__C"></div>
-                        <div className="cube__face corner__D"></div>
-                        <div className="cube__face cube__face--right">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--left">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__E"></div>
-                        <div className="cube__face corner__F"></div>
-                        <div className="cube__face cube__face--top">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face cube__face--bottom">
-                            <div className="cube__face"></div>
-
-                        </div>
-                        <div className="cube__face corner__G"></div>
-                        <div className="cube__face corner__H"></div>
-                    </div>
-                </div>
+                <Cube></Cube>
             </article>
         </section>
     );
