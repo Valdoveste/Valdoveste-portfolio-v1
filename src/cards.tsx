c6b932ff16a2d1c054939edd8985a42f98153fa7
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 02:19:17 -0300

    Better object information

--- src/cards.tsx
+++ src/cards.tsx
@@ -1,23 +1,26 @@
 const PortfolioCards = [
     {
         id: 0,
-        logo_alt: "Nebulum Logo",
-        url: "https://github.com/Valdoveste/nebulum",
-        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png",
+        project_url: "https://nebulum.000webhostapp.com/",
+        project_git_url: "https://github.com/Valdoveste/nebulum",
+        project_img_alt: "Nebulum Project Image",
+        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
         projec_desc: "An online album with the objective to be simple, clean, and straight to the point. Consuming the Pexels API to load a massive amount of different photos. One of the main objectives of this application was to learn more about clean & simple design also have extra care for the mobile users, applying concepts as infinityScroll and FAB(Floating Action Button) and modal that works for portrait and landscape screen orientations."
     },
     {
         id: 1,
-        logo_alt: "Nebulum Logo",
-        url: "https://github.com/Valdoveste/nebulum",
-        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png",
+        project_url: "https://nebulum.000webhostapp.com/",
+        project_git_url: "https://github.com/Valdoveste/nebulum",
+        project_img_alt: "Nebulum Project Image",
+        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
         projec_desc: "An online album with the objective to be simple, clean, and straight to the point. Consuming the Pexels API to load a massive amount of different photos. One of the main objectives of this application was to learn more about clean & simple design also have extra care for the mobile users, applying concepts as infinityScroll and FAB(Floating Action Button) and modal that works for portrait and landscape screen orientations."
     },
     {
         id: 2,
-        logo_alt: "Nebulum Logo",
-        url: "https://github.com/Valdoveste/nebulum",
-        logo: "https://user-images.githubusercontent.com/62577482/145720044-3426d797-c4fd-4f23-84ce-1627ce668420.png",
+        project_url: "https://nebulum.000webhostapp.com/",
+        project_git_url: "https://github.com/Valdoveste/nebulum",
+        project_img_alt: "Nebulum Project Image",
+        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
         projec_desc: "An online album with the objective to be simple, clean, and straight to the point. Consuming the Pexels API to load a massive amount of different photos. One of the main objectives of this application was to learn more about clean & simple design also have extra care for the mobile users, applying concepts as infinityScroll and FAB(Floating Action Button) and modal that works for portrait and landscape screen orientations."
     },
 ];
