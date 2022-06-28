0877e34d1ec9ab1638c8fbdf664dddd7ddd53a14
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 02:14:39 -0300

    Function refactored

--- src/Effects/ByTimeElementAnimation.tsx
+++ src/Effects/ByTimeElementAnimation.tsx
@@ -1,22 +1,21 @@
-function ByTimeElementAnimation(Element: string, Animation: string, Direction: string) {
+function ByTimeElementAnimation(Element: string, Animation: string, Order: string, Time: number) {
     const Elements = document.querySelectorAll(Element);
-    var clock = setInterval(anime, 500);
-    var clockState: number = 0;
+    var clock = setInterval(anime, Time);
+    var clockState = (Elements.length - 1);
 
-    if (Direction == "asc")
-        clockState = (Elements.length - 1);
+    if (Order === "asc")
+        clockState = 0;
 
-        console.log(Elements)
     function anime() {
-        if (Direction == "desc") {
+        if (Order === "desc") {
             if (clockState === -1) {
                 clearInterval(clock)
             } else {
                 Elements[clockState].classList.add(Animation)
                 clockState--;
             }
-        } else if (Direction == "asc") {
-            if (clockState < Elements.length) {
+        } else if (Order === "asc") {
+            if (clockState > (Elements.length - 1)) {
                 clearInterval(clock)
             } else {
                 Elements[clockState].classList.add(Animation)
