e12ea678a7763a965b862e8b9661c118f243445d
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 02:09:00 -0300

    Cube component with sass

---
+++ src/components/Cube/Cubes.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+const Cube: React.FC = () => {
+    return (
+        <>
+            <div className="scene2">
+                <div className="cube__outter">
+                    <div className="cube__face cube__face--front">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__A"></div>
+                    <div className="cube__face corner__B"></div>
+                    <div className="cube__face cube__face--back">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__C"></div>
+                    <div className="cube__face corner__D"></div>
+                    <div className="cube__face cube__face--right">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--left">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__E"></div>
+                    <div className="cube__face corner__F"></div>
+                    <div className="cube__face cube__face--top">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--bottom">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__G"></div>
+                    <div className="cube__face corner__H"></div>
+                </div>
+            </div>
+
+            <div className="scene1">
+                <div className="cube__inner">
+                    <div className="cube__face cube__face--front">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__A"></div>
+                    <div className="cube__face corner__A"></div>
+                    <div className="cube__face corner__B"></div>
+                    <div className="cube__face cube__face--back">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__C"></div>
+                    <div className="cube__face corner__D"></div>
+                    <div className="cube__face cube__face--right">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--left">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__E"></div>
+                    <div className="cube__face corner__F"></div>
+                    <div className="cube__face cube__face--top">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--bottom">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__G"></div>
+                    <div className="cube__face corner__H"></div>
+                </div>
+            </div>
+
+            <div className="scene3">
+                <div className="cube__inner__inner">
+                    <div className="cube__face cube__face--front">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__A"></div>
+                    <div className="cube__face corner__B"></div>
+                    <div className="cube__face cube__face--back">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__C"></div>
+                    <div className="cube__face corner__D"></div>
+                    <div className="cube__face cube__face--right">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--left">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__E"></div>
+                    <div className="cube__face corner__F"></div>
+                    <div className="cube__face cube__face--top">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face cube__face--bottom">
+                        <div className="cube__face"></div>
+
+                    </div>
+                    <div className="cube__face corner__G"></div>
+                    <div className="cube__face corner__H"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Cube;
