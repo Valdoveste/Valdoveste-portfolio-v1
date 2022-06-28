1f743673b559ad9c564f7782995b58fe71bbdf1f
Valdoveste <caio.valdoveste@outlook.com.br>
2022-06-28 01:58:51 -0300

    Fadein effect on the home elements

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,37 +1,54 @@
 import React from "react";
-import IntroG from "../../assets/IntroG.gif";
-// import Intro from "../../assets/IntroV.mp4";
+import scrollTo from "../../Effects/ScrollTo"
 
 const Home: React.FC = () => {
 
+    const styles = {
+        firstTransiton: {
+            animationDelay: "50ms",
+        },
+        secondTransiton: {
+            animationDelay: "100ms",
+        },
+        thirdTransiton: {
+            animationDelay: "150ms",
+        },
+        fourthTransiton: {
+            animationDelay: "200ms",
+        },
+        fifthTransiton: {
+            animationDelay: "250ms",
+        },
+    };
 
     return (
-        <section id="home" style={{ backgroundImage: `url(${IntroG})` }}>
-            {/* <img src={IntroG} loading="lazy" alt="a" className="home__image"/> */}
-            {/* <video key={1} className="home__image" controls>
-                <source src={Intro} type="video/mp4"></source>
-                Your browser does not support this video, video source below.
-                https://pt.videezy.com/fundos/5054-fluxo-de-fundo-de-movimento-4k-das-chamas
-            </video> */}
+        <section id="home">
             <article className="home__content">
-                <h1 className="home__content__title">
+                <h1 className="home__content__title --fadeinup" style={styles.firstTransiton}>
                     Hi,
                 </h1>
 
-                <h1 className="home__content__title">
-                    I'm
-                    &nbsp;
-                    <span>
-                        Valdoveste
-                    </span>
+                <h1 className="home__content__title --fadeinup" style={styles.secondTransiton}>
+                    I'm Valdoveste
                 </h1>
-                <p className="home__content__text">
+                <p className="home__content__text --fadeinup" style={styles.thirdTransiton}>
                     FullStack Developer
                 </p>
+                <p className="home__content__text --fadeinup" style={styles.fourthTransiton}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque lobortis eleifend tellus, ut fermentum mauris luctus ac.
+                    Cras finibus quis ante id interdum. Nam eget leo metus. Donec tincidunt magnaneque, vitae accumsan augue consequat eget.
+                </p>
+
+                <a href="/contact" onClick={scrollTo} className="btn-section --fadeinup" style={styles.fifthTransiton}>
+                    Get In Touch
+                    <span>
+                    </span>
+                </a>
+
             </article>
         </section>
     );
 }
-
 
 export default Home;
