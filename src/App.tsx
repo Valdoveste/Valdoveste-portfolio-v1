1ff2e941ed36cca5aeaafd0c6e6489a24547bba6
Valdoveste <caio.valdoveste@outlook.com.br>
2022-07-08 23:57:42 -0300

    Observers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
@@ -6,26 +7,77 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './styles/styles.css'
 
-window.addEventListener("load", (e) => {
-  const sections = document.querySelectorAll('section')!;
+const App: React.FC = () => {
+  window.addEventListener("DOMContentLoaded", (e) => {
 
-  const observerSection = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.intersectionRatio > 0) {
-        var target = entry.target;
-        if (!target.classList.contains('--fadeinup') && target.id !== "home") {
-          target.classList.add('--fadeinup');
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    const options = {
+      root: null,
+      threshold: 0.2,
+      marginRoot: "0px"
+    }
+
+    // Cards Observer
+    const observerCards = new IntersectionObserver((entries, observerCards) => {
+      entries.forEach(entry => {
+        let target = entry.target;
+        if (entry.intersectionRatio > 0) {
+          if (mediaQuery.matches) {
+            target.classList.add("--fadeinup");
+          } else {
+            if (parseInt(target.id) % 2 === 0) {
+              target.classList.add("--fadetoleft");
+            }
+            else {
+              target.classList.add("--fadetoright");
+            }
+          }
         }
-      }
+      });
     });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+      observerCards.observe(card);
+      card.addEventListener("animationend", (e) => {
+        card.classList.add("--opacity");
+      });
+    });
+
+    // Section Observer
+    const oberserSection = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        let target = entry.target;
+        if (entry.intersectionRatio > 0 && target.id !== "home")
+          target.classList.add("--fadeinup");
+      });
+    });
+
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+      oberserSection.observe(section);
+    });
+
+
+    // Cube Observer
+    const aboutCube = document.getElementById('about')!;
+
+    const observerCube = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          document.querySelectorAll('.scene1')[0].classList.add("--fadeincube");
+          document.querySelectorAll('.scene2')[0].classList.add("--fadeincube");
+          document.querySelectorAll('.scene3')[0].classList.add("--fadeincube");
+        }
+      });
+    }, options);
+
+    observerCube.observe(aboutCube);
   });
 
-  sections.forEach(section => {
-    observerSection.observe(section)
-  })
-})
-
-function App() {
   return (
     <>
       <Header></Header>
