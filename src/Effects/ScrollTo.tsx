e6bd273a70d3f6b6ac5e349c44e6f1a2b8f4de8d
Valdoveste <caio.valdoveste@outlook.com.br>
2022-08-18 19:35:44 -0300

    Better ScrollTo function

--- src/Effects/ScrollTo.tsx
+++ src/Effects/ScrollTo.tsx
@@ -1,9 +1,15 @@
-const scrollTo = (event : React.MouseEvent<HTMLAnchorElement>) => {
+import React from "react";
+
+const scrollTo = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     let target = event.currentTarget.pathname.split("/")[1];
-
     let dest = document.getElementById(target)!;
-    window.scrollTo(dest.offsetLeft, dest.offsetTop);
+
+    if (target === "home")
+        window.scrollTo(0, 0);
+    else
+        window.scrollTo(dest.offsetLeft, dest.offsetTop);
+
 }
 
 export default scrollTo;
