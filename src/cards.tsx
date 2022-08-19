f09f14f18a169a62828dd8e5466d0ed903c60f6b
Valdoveste <caio.valdoveste@outlook.com.br>
2022-08-19 18:02:13 -0300

    Outdated info comented

--- src/cards.tsx
+++ src/cards.tsx
@@ -7,22 +7,6 @@ const PortfolioCards = [
         project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
         projec_desc: "Um álbum de fotos online, desenvolvimento deste projeto teve como objetivo minhas habilidades com clean design, javascript e cosumo de API's. Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile. Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll."
     },
-    {
-        id: 1,
-        project_url: "https://nebulum.000webhostapp.com/",
-        project_git_url: "https://github.com/Valdoveste/nebulum",
-        project_img_alt: "Nebulum Project Image",
-        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
-        projec_desc: "Um álbum de fotos online, desenvolvimento deste projeto teve como objetivo minhas habilidades com clean design, javascript e cosumo de API's. Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile. Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll."
-    },
-    {
-        id: 2,
-        project_url: "https://nebulum.000webhostapp.com/",
-        project_git_url: "https://github.com/Valdoveste/nebulum",
-        project_img_alt: "Nebulum Project Image",
-        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
-        projec_desc: "Um álbum de fotos online, desenvolvimento deste projeto teve como objetivo minhas habilidades com clean design, javascript e cosumo de API's. Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile. Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll."
-    },
 ];
 
 export default PortfolioCards;
