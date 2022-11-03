ac447f20bf890f98a42d0e8876a92186290bda64
Valdoveste <caio.valdoveste@outlook.com.br>
2022-11-02 22:58:35 -0300

    Orthography corretion

--- src/cards.tsx
+++ src/cards.tsx
@@ -5,7 +5,7 @@ const PortfolioCards = [
         project_git_url: "https://github.com/Valdoveste/nebulum",
         project_img_alt: "Nebulum Project Image",
         project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
-        projec_desc: "Um álbum de fotos online, desenvolvimento deste projeto teve como objetivo minhas habilidades com clean design, javascript e cosumo de API's. Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile. Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll."
+        projec_desc: "Um álbum de fotos online, onde uma dos principais objetivos durante seu desenvolvimento praticar minhas habilidades com clean design, javascript e com o consumo de API's.Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile.Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll."
     },
 ];
 
