b638381c64509e3879438cc2ec36e5ad36fc1997
Valdoveste <caio.valdoveste@outlook.com.br>
2022-11-02 22:57:36 -0300

    renderToString on App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as ReactDOMServer from 'react-dom/server';
 
+ReactDOMServer.renderToString(<App />)
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
