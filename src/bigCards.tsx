a19d90342a5eebe9eb301ef285e6f61ae8deea66
Valdoveste <caio.valdoveste@outlook.com.br>
2023-03-11 14:20:53 -0300

    Small and Big Cards

---
+++ src/bigCards.tsx
@@ -0,0 +1,26 @@
+const PortfolioBigCards = [
+    {
+        id: Math.random(),
+        layout: "big",
+        project_name: "Nebulum",
+        project_url: "https://valdoveste.github.io/nebulum/",
+        project_git_url: "https://github.com/Valdoveste/nebulum",
+        project_img_alt: "Nebulum Project Image",
+        project_img: "https://user-images.githubusercontent.com/62577482/145719124-01abeffd-3a16-489d-b9be-a5ea9e68c243.png",
+        project_desc: "Um álbum de fotos online, onde uma dos principais objetivos durante seu desenvolvimento praticar minhas habilidades com clean design, javascript e com o consumo de API's. Um dos grande desafios durante o desenvolvimento foi tratar a imagens e dar uma preocupação extra para usuário mobile. Durante o tratamento das imagens foi utilizado loading lazy, placeholder baseado em sua cor média, e uma das funcionalidades para mobile é sua paginação que funciona com infinityScroll.",
+        project_tech: ["JAVASCRIPT", "HTML5", "CSS3", "SASS"]
+    },
+    {
+        id: Math.random(),
+        layout: "big",
+        project_name: "Five Senses Resorts",
+        project_url: "https://valdoveste.github.io/fiveSensesResort/",
+        project_git_url: "https://github.com/Valdoveste/fiveSensesResort",
+        project_img_alt: "Fives Senses Resort Project Image",
+        project_img: "https://user-images.githubusercontent.com/62577482/224366779-760839c3-bddf-4f6f-9cd3-f965b773d2c3.png",
+        project_desc: "Five Senses Resort é uma apliação web utilizando o framework bootstrap, onde o objetivo foi montar um página que apresente as principais informações do primeiro Resort de Tocantins, o Five Senses veio com a necessidade do lazer no cotidiano do tocantinese em aproveitar o tempo em família e no descanso. A beleza natural do lago de Palmas e os encantos naturais da região criaram esse desejo de proporcionar um novo destino de férias. ",
+        project_tech: ["JAVASCRIPT", "HTML5", "CSS3", "SASS", "BOOTSTRAP"]
+    }
+];
+
+export default PortfolioBigCards;
---
+++ src/smallCards.tsx
@@ -0,0 +1,70 @@
+const PortfolioSmallCards = [
+    {
+        id: Math.random(),
+        layout: "small",
+        project_name: "RotmgTargetCreator",
+        project_url: "https://valdoveste.github.io/rotmgex-target-creator/",
+        project_git_url: "https://github.com/Valdoveste/rotmgex-target-creator",
+        project_img_alt: "RotmgTargetCreator Project Image",
+        project_desc: "Facilitator for those that have mules and have no patience to create write target in base64.",
+        project_tech: ["HTML5", "CSS3", "JS"],
+        animation_delay: {
+            animationDelay: "550ms"
+        }
+    },
+    {
+        id: Math.random(),
+        layout: "small",
+        project_name: "BatatinhasHardware",
+        project_url: "https://batatinha-s-hardware.github.io/batatinhas-hardware/",
+        project_git_url: "https://github.com/Batatinha-s-Hardware/batatinhas-hardware",
+        project_img_alt: "BatatinhasHardware Project Image",
+        project_desc: "Batatinha Hardware, é uma loja virtual fictícia especializada no mercado eletrônico, vendemos aparelhos e acessórios de diversas marcas, sempre buscando entregar omelhor para nosso cliente.",
+        project_tech: ["HTML5", "CSS3", "JS", "JQUERY"],
+        animation_delay: {
+            animationDelay: "450ms"
+        }
+    },
+    {
+        id: Math.random(),
+        layout: "small",
+        project_name: "NoSlepetDebuff",
+        project_url: "",
+        project_git_url: "https://github.com/Valdoveste/NoSleptDebuff",
+        project_img_alt: "NoSleptDebuff Project Image",
+        project_desc: "Esse projeto junto com os dois próximos fazem parte de um experimento que me propus com o intuito de relembrar alguns conceitos de Java e entender o processo de desenvolvimento de plugins para servidores do jogo Minecraft. Um dos conhecimentos essências para a finalização da primeira versão deste projeto foi a interação com eventos, criação de tasks/thread, Wrappers, criação e manipulação de arquivos com  Java File",
+        project_tech: ["JAVA", "SPIGOT", "MINECRAFT PLUGIN"],
+        animation_delay: {
+            animationDelay: "350ms"
+        }
+    },
+    {
+        id: Math.random(),
+        layout: "small",
+        project_name: "ItemFramesAppearance",
+        project_url: "",
+        project_git_url: "https://github.com/Valdoveste/ItemFramesAppearance",
+        project_img_alt: "ItemFramesAppearance Project Image",
+        project_desc: "Esse projeto foi desenvolvido com o intuito de relembrar alguns conceitos de Java e entender o processo de desenvolvimento de plugins para servidores do jogo Minecraft. Um dos conhecimentos essências para a finalização deste projeto deste projeto foi a interação com eventos e criação e manipulação de arquivos com  Java File",
+        project_tech: ["JAVA", "SPIGOT", "MINECRAFT PLUGIN"],
+        animation_delay: {
+            animationDelay: "250ms"
+        }
+    },
+    {
+        id: Math.random(),
+        layout: "small",
+        project_name: "DeleteUUIDOnDeath",
+        project_url: "",
+        project_git_url: "https://github.com/Valdoveste/DeleteUUIDOnDeath",
+        project_img_alt: "DeleteUUIDOnDeath Project Image",
+        project_desc: "Esse projeto foi desenvolvido com o intuito de relembrar alguns conceitos de Java e entender o processo de desenvolvimento de plugins para servidores do jogo Minecraft. Um dos conhecimentos essências para a finalização deste projeto deste projeto foi a interação com eventos e criação e manipulação de arquivos com  Java File",
+        project_tech: ["JAVA", "SPIGOT", "MINECRAFT PLUGIN"],
+        animation_delay: {
+            animationDelay: "150ms"
+        }
+    }
+];
+
+
+export default PortfolioSmallCards;
